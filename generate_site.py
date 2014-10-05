e2f1e2ec143a671f2d171d73de989176236b42b1
Ricardo Lafuente <r@manufacturaindependente.org>
2014-10-05 13:26:20 +0100

    Primeira tentativa de render estático das transcrições

--- generate_site.py
+++ generate_site.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import urllib2
-import json
+import csv
 from datetime import date
-from pprint import pprint
-import os, shutil
+from dateutil import parser as dateparser
+import os
+import codecs
+import shutil
+import markdown
+from zenlog import log
 
 MESES = [
     'Janeiro',
@@ -21,7 +24,7 @@ MESES = [
     'Dezembro',
     ]
 
-VIEW_URL = "http://koizo.org:5984/dar-1_datas/_design/dar/_view/all_dates"
+DATASETS_DIR = "~/Datasets/"
 OUTPUT_DIR = "_output"
 MEDIA_SOURCE_DIR = "_media"
 
@@ -29,14 +32,13 @@ MEDIA_SOURCE_DIR = "_media"
 # 1. PREPARE DATA #
 ###################
 
+DATASETS_DIR = os.path.expanduser(DATASETS_DIR)
 # fetch all dates
-page = urllib2.urlopen(VIEW_URL)
-data = json.loads(page.read())
-rows = data['rows']
-
+data = csv.reader(open(os.path.join(DATASETS_DIR, 'dar-datas.csv'), 'r'))
+# skip header
+data.next()
 # process dates into a year->dates dict
-from dateutil import parser
-all_dates = [parser.parse(row['key']) for row in rows]
+all_dates = [dateparser.parse(row[3]) for row in data]
 all_years = list(set([d.year for d in all_dates]))
 
 datedict = {}
@@ -47,14 +49,15 @@ for year in all_years:
         month = date.today().month
         months = range(1, month+1)
     else:
-        months = range(1,13)
+        months = range(1, 13)
     datedict[year] = {}
     for month in months:
         datedict[year][month] = {}
         import calendar
         days_in_month = calendar.monthrange(year, month)[-1]
         all_days = range(1, days_in_month+1)
-        session_days = [date(d.year, d.month, d.day) for d in all_dates if d.month == month and d.year == year]
+        session_days = [date(d.year, d.month, d.day) for d in all_dates
+                        if d.month == month and d.year == year]
         for day_number in all_days:
             day_date = date(year, month, day_number)
             if day_date in session_days:
@@ -64,12 +67,14 @@ for year in all_years:
             datedict[year][month][day_number] = {'weekday': day_date.weekday(),
                                                  'has_session': has_session}
 
-# FIXME, this is stupid
-all_years = datedict
-
 ##########################
 # 2. GENERATE HTML FILES #
 ##########################
+
+# flush output dir
+if os.path.exists(OUTPUT_DIR):
+    shutil.rmtree(OUTPUT_DIR)
+os.mkdir(OUTPUT_DIR)
 
 # init Jinja
 from jinja2 import Environment, PackageLoader
@@ -77,42 +82,79 @@ env = Environment(loader=PackageLoader('democratica', 'templates'),
                   extensions=['jinja2htmlcompress.SelectiveHTMLCompress'],
                   trim_blocks=True, lstrip_blocks=True)
 
-## HOME PAGE ##
+# HOME PAGE #
 
 template = env.get_template('index.html')
 html = template.render()
 filename = "index.html"
-import os, codecs
 outfile = codecs.open(os.path.join(OUTPUT_DIR, filename), 'w', 'utf-8')
 outfile.write(html)
 outfile.close()
-print "%s created OK" % filename
+log.debug("%s created OK" % filename)
 
-## SESSION CALENDAR ##
+# SESSION CALENDAR #
 
+log.info("Generating session calendar...")
 template = env.get_template('transcripts/day_list.html')
-for year_number in all_years:
-    year = all_years[year_number]
+for year_number in datedict:
+    year = datedict[year_number]
     context = {'year': year,
                'year_number': year_number,
-               'all_years': all_years,
+               'datedict': datedict,
                }
     html = template.render(**context)
     filename = "%s.html" % year_number
-    import os, codecs
     outfile = codecs.open(os.path.join(OUTPUT_DIR, filename), 'w', 'utf-8')
     outfile.write(html)
     outfile.close()
-    print "%s created OK" % filename
-    
-## STATIC FILES ##
+    # log.debug("%s created OK" % filename)
 
+# STATIC FILES #
+
+log.info("Regenerating static files...")
 # regenerate static content dir (css, js, images, fonts)
 media_path = os.path.join(OUTPUT_DIR, 'media')
 if os.path.exists(media_path):
     shutil.rmtree(media_path)
 os.mkdir(media_path)
-# copy_tree method lifted from user prosseek http://stackoverflow.com/a/15034373
+# copy_tree method lifted from http://stackoverflow.com/a/15034373
 import distutils.core
 distutils.dir_util.copy_tree(MEDIA_SOURCE_DIR, media_path)
+
+# INDIVIDUAL SESSIONS #
+
+template = env.get_template('transcripts/day_detail_markdown.html')
+log.info("Generating HTML session pages...")
+sessions_dir = "sessoes/"
+os.mkdir(os.path.join(OUTPUT_DIR, sessions_dir))
+# fetch all dates
+data = csv.reader(open(os.path.join(DATASETS_DIR, 'dar-datas.csv'), 'r'))
+# skip header
+data.next()
+for leg, sess, num, d, dpub in data:
+    filename = d + '.html'
+    if 'S' in num:
+        sourcefile = "%02d-%d-%s.txt" % (int(leg), int(sess), num)
+    else:
+        sourcefile = "%02d-%d-%03d.txt" % (int(leg), int(sess), int(num))
+    sourcepath = os.path.join(
+        (DATASETS_DIR), 'dar-transcricoes-txt', sourcefile)
+    text = codecs.open(sourcepath, 'r', 'utf-8').read()
+    html_text = markdown.markdown(text)
+    context = {'session_date': dateparser.parse(d),
+               'year_number': year_number,
+               'text': html_text,
+               'pdf_url': 'xpto',
+               }
+    html = template.render(**context)
+    outfile = codecs.open(os.path.join(OUTPUT_DIR, filename), 'w', 'utf-8')
+    outfile.write(html)
+    outfile.close()
+    log.debug(filename)
+
+    # sourcepath = os.path.join(DATASETS_DIR,
+    #                          "dar-transcricoes-html/", sourcefile)
+    # targetpath = os.path.join(OUTPUT_DIR, sessions_dir, filename)
+    # shutil.copyfile(sourcepath, targetpath)
+
 
