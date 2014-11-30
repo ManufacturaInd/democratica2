c9fec8b86c93f2271fce683abcdb9fb61833682e
Ricardo Lafuente <r@manufacturaindependente.org>
2014-11-30 19:20:04 +0000

    Adicionar lista de deputados, reorg dos templates

--- generate_site.py
+++ generate_site.py
@@ -1,29 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from distutils.dir_util import copy_tree
-import csv
-from datetime import date
-from dateutil import parser as dateparser
 import os
 import codecs
 import shutil
-import hoedown as markdown
+# import click
 from zenlog import log
 
+from distutils.dir_util import copy_tree
+from datetime import date
+from dateutil import parser as dateparser
+import unicodecsv as csv
+import hoedown as markdown
 
-DATASET_FILE = "/home/rlafuente/Datasets/parlamento-datas/data/parlamento-datas.csv"
+
+TRANSCRIPT_DATASET_FILE = "/home/rlafuente/Datasets/parlamento-datas/data/parlamento-datas.csv"
 TRANSCRIPTS_DIR = "/home/rlafuente/Datasets/dar-transcricoes-txt/"
+MP_DATASET_FILE = "/home/rlafuente/Datasets/parlamento-deputados/data/deputados.csv"
 OUTPUT_DIR = "_output"
 MEDIA_SOURCE_DIR = "_media"
 MEDIA_PATH = "media/"
 TRANSCRIPTS_PATH = "sessoes/"
+MPS_PATH = "deputados/"
 
 MESES = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho',
          'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
 
 
 def get_date_dataset():
-    data = csv.reader(open(DATASET_FILE, 'r'))
+    data = csv.reader(open(TRANSCRIPT_DATASET_FILE, 'r'))
+    # skip first row
+    data.next()
+    return data
+
+
+def get_mp_dataset():
+    data = csv.reader(open(MP_DATASET_FILE, 'r'))
     # skip first row
     data.next()
     return data
@@ -62,6 +73,39 @@ def generate_datedict():
     return datedict
 
 
+def generate_mp_list(only_active=True):
+    mps = []
+    data = get_mp_dataset()
+    for row in data:
+        is_active = row[4]
+        if is_active == "False":
+            is_active = False
+        else:
+            is_active = True
+        if only_active and not is_active:
+            continue
+
+        # id, shortname, name, party, is_active, education, birthdate, occupation, current_jobs, jobs,\
+        # commissions, mandates, awards, url, scrape_date
+        mps.append({
+            "id": row[0],
+            "shortname": row[1],
+            "name": row[2],
+            "party": row[3],
+            "is_active": row[4],
+            "education": row[5],
+            "birth_date": row[6],
+            "occupation": row[7],
+            "current_jobs": row[8],
+            "jobs": row[9],
+            "commissions": row[10],
+            "mandates": row[11],
+            "awards": row[12],
+            "url": row[13],
+        })
+    return mps
+
+
 def render_template_into_file(env, templatename, filename, context=None, place_in_outdir=True):
     template = env.get_template(templatename)
     if not context:
@@ -95,10 +139,12 @@ def delete_and_create_dir(d):
     os.mkdir(d)
 
 
+# @click.command()
 def generate_site():
     # flush output
     delete_and_create_dir(OUTPUT_DIR)
     delete_and_create_dir(os.path.join(OUTPUT_DIR, TRANSCRIPTS_PATH))
+    delete_and_create_dir(os.path.join(OUTPUT_DIR, MPS_PATH))
     delete_and_create_dir(os.path.join(OUTPUT_DIR, MEDIA_PATH))
 
     # init Jinja
@@ -111,7 +157,12 @@ def generate_site():
     log.info("Generating index...")
     render_template_into_file(env, 'index.html', 'index.html')
 
-    log.info("Generating session calendar...")
+    log.info("Generating MP index...")
+    mps = generate_mp_list()
+    context = {"mps": mps}
+    render_template_into_file(env, 'mp_list.html', "deputados/index.html", context)
+
+    log.info("Generating session index...")
     datedict = generate_datedict()
     for year_number in datedict:
         year = datedict[year_number]
@@ -120,10 +171,11 @@ def generate_site():
                    'datedict': datedict,
                    }
         filename = "%s.html" % year_number
-        render_template_into_file(env, 'transcripts/day_list.html', filename, context)
+        render_template_into_file(env, 'day_list.html', filename, context)
 
     log.info("Generating HTML session pages...")
     data = get_date_dataset()
+    counter = 0
     for leg, sess, num, d, dpub in data:
         context = {'session_date': dateparser.parse(d),
                    'year_number': year_number,
@@ -131,8 +183,11 @@ def generate_site():
                    'pdf_url': 'xpto',
                    }
         filename = os.path.join(TRANSCRIPTS_PATH, d + '.html')
-        render_template_into_file(env, 'transcripts/day_detail_markdown.html', filename, context)
+        render_template_into_file(env, 'day_detail.html', filename, context)
         log.debug(d)
+        counter += 1
+        if counter > 30:
+            break
 
     log.info("Copying static files...")
     copy_tree(MEDIA_SOURCE_DIR, os.path.join(OUTPUT_DIR, MEDIA_PATH))
