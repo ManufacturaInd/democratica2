c24cb72784cfb1cf4aaa29e46c2e673158d0a3aa
aiscarvalho <a@manufacturaindependente.org>
2016-11-19 13:31:15 +0000

    tirei blocos de jquery antigos -- uniform forms e tooltips tipsy --; inclui o wirep

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var connect = require('gulp-connect');
+var wiredep = require('wiredep').stream;
 
 gulp.task('connect', function(){
   connect.server({
@@ -10,29 +11,30 @@ gulp.task('connect', function(){
 });
 
 gulp.task('sass', function () {
-  return gulp.src('./assets/sass/**/*.scss')
+  return gulp.src('assets/sass/**/*.scss')
       .pipe(sass({ 
         errLogToConsole: true,
         sourceComments: true,
         includePaths: ['bower_components/foundation/scss']
       }).on('error', sass.logError))
-      .pipe(gulp.dest('./public/assets/css'));
+      .pipe(gulp.dest('dist/assets/css'));
 });
 
 gulp.task('fonts', function () {
-  return gulp.src('./assets/fonts/**/*.*')
-      .pipe(gulp.dest('./public/assets/fonts'));
+  return gulp.src('assets/fonts/**/*.*')
+      .pipe(gulp.dest('dist/assets/fonts'));
 });
 
 gulp.task('img', function () {
-  return gulp.src('./assets/img/**/*.*')
-      .pipe(gulp.dest('./public/assets/img'));
+  return gulp.src('assets/img/**/*.*')
+      .pipe(gulp.dest('dist/assets/img'));
 });
 
 gulp.task('scripts', function () {
-  return gulp.src('./assets/scripts/**/*.js')
-      .pipe(gulp.dest('./public/assets/scripts'));
+  return gulp.src('assets/scripts/**/*.js')
+      .pipe(gulp.dest('dist/assets/scripts'));
 });
+
 
 gulp.task('livereload', function (){
   return gulp.src('./public/**/*')
@@ -40,10 +42,32 @@ gulp.task('livereload', function (){
 });
 
 gulp.task('watch', function () {
-  gulp.watch('./assets/sass/**/*.scss', ['sass']);
-  gulp.watch('./assets/img/**/*.*', ['img']);
-  gulp.watch('./assets/scripts/*.js', ['scripts']);
-  gulp.watch('./public/**/**/*', ['livereload']);
+  gulp.watch('assets/sass/**/*.scss', ['sass']);
+  gulp.watch('assets/img/**/*.*', ['img']);
+  gulp.watch('assets/scripts/*.js', ['scripts']);
+  gulp.watch('dist/**/**/*', ['livereload']);
+});
+
+// Inject Bower components
+gulp.task('wiredep', function () {
+    gulp.src('assets/sass/*.scss')
+        .pipe(wiredep({
+            directory: 'bower_components',
+            ignorePath: 'bower_components/'
+        }))
+        .pipe(gulp.dest('assets/sass'));
+    gulp.src('assets/scripts/*.js')
+        .pipe(wiredep({
+            directory: 'bower_components',
+            ignorePath: 'bower_components/'
+        }))
+        .pipe(gulp.dest('assets/scripts'));
+    gulp.src('templates/**/*.html')
+        .pipe(wiredep({
+            directory: 'bower_components',
+            ignorePath: 'templates/'
+        }))
+        .pipe(gulp.dest('templates'));
 });
 
 gulp.task('build', ['sass', 'fonts', 'img', 'scripts']);
