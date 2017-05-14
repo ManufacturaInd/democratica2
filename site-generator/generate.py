ead87aabafdccd4b84eabe91487636b3000fa2d4
Ricardo Lafuente <r@manufacturaindependente.org>
2017-05-14 20:22:22 +0100

    Refactorizar passos da geração antes de passar pra classes

--- site-generator/generate.py
+++ site-generator/generate.py
@@ -28,6 +28,12 @@ env = jinja2.Environment(loader=jinja2.FileSystemLoader([TEMPLATE_DIR]),
 env.filters['date'] = format_date
 md = markdown.Markdown(extensions=['meta'])
 
+mps = generate_mp_list(only_active=False)
+gov_data = get_gov_dataset()
+govpost_data = list(get_govpost_dataset())
+gov_mp_ids = [int(row[2]) for row in govpost_data if row[2]]
+date_data = get_date_dataset()
+
 
 def render_template_into_file(env, templatename, filename, context=None, place_in_outdir=True):
     target_dir = os.path.join(OUTPUT_DIR, os.path.dirname(filename))
@@ -50,44 +56,11 @@ def generate_homepage():
 
 
 def generate_mp_index():
-    pass
-
-
-def generate_mp_page(mp):
-    pass
-
-
-def generate_session_index():
-    pass
-
-
-def generate_session_page():
-    pass
-
-
-@click.option("-f", "--fast-run", help="Generate only a few transcripts to save time", is_flag=True, default=False)
-@click.command()
-def generate_site(fast_run):
-    # Create output dirs
-    create_dir(OUTPUT_DIR)
-    create_dir(os.path.join(OUTPUT_DIR, TRANSCRIPTS_PATH))
-    create_dir(os.path.join(OUTPUT_DIR, MPS_PATH))
-
-    # Generate the site!
-    log.info("Generating index...")
-    generate_homepage()
-
-    log.info("Generating MP index...")
-    mps = generate_mp_list(only_active=False)
     context = {"mps": mps, 'page_name': 'deputados'}
     render_template_into_file(env, 'mp_list.html', "deputados/index.html", context)
 
-    gov_data = get_gov_dataset()
-    govpost_data = list(get_govpost_dataset())
-    gov_mp_ids = [int(row[2]) for row in govpost_data if row[2]]
-    date_data = get_date_dataset()
 
-    log.info("Generating MP pages...")
+def generate_mp_pages():
     for mp in mps:
         id = int(mp['id'])
         mp['photo_url'] = PHOTO_URL_BASE + str(id) + ".jpg"
@@ -124,7 +97,38 @@ def generate_site(fast_run):
         filename = os.path.join(MPS_PATH, mp['slug'], 'index.html')
         render_template_into_file(env, 'mp_detail.html', filename, context)
 
-    log.info("Generating HTML session pages...")
+
+def generate_session_index():
+    datedict = generate_datedict()
+    all_years = [y for y in datedict]
+    for year_number in datedict:
+        year = datedict[year_number]
+        context = {'year': year,
+                   'year_number': year_number,
+                   'all_years': all_years,
+                   'datedict': datedict,
+                   'months': MESES,
+                   'months_short': [m[:3] for m in MESES],
+                   'page_name': 'sessoes',
+                   }
+        target_dir = os.path.join(TRANSCRIPTS_PATH + "%s/" % year_number)
+        filename = target_dir + "index.html"
+        render_template_into_file(env, 'session_list.html', filename, context)
+    # Get most recent year and make the session homepage (when you open the "sessions" tab)
+    y = all_years[-1]
+    year = datedict[y]
+    context = {'year': year,
+               'year_number': year_number,
+               'all_years': all_years,
+               'datedict': datedict,
+               'months': MESES,
+               'months_short': [m[:3] for m in MESES],
+               'page_name': 'sessoes',
+               }
+    render_template_into_file(env, 'session_list.html', TRANSCRIPTS_PATH + 'index.html', context)
+
+
+def generate_session_pages(fast_run=False):
     if fast_run:
         COUNTER = 0
     date_data.reverse()
@@ -166,34 +170,30 @@ def generate_site(fast_run):
             if COUNTER > FAST_RUN_COUNT:
                 break
 
+
+@click.option("-f", "--fast-run", help="Generate only a few transcripts to save time", is_flag=True, default=False)
+@click.command()
+def generate_site(fast_run):
+    # Create output dirs
+    create_dir(OUTPUT_DIR)
+    create_dir(os.path.join(OUTPUT_DIR, TRANSCRIPTS_PATH))
+    create_dir(os.path.join(OUTPUT_DIR, MPS_PATH))
+
+    # Generate the site!
+    log.info("Generating index...")
+    generate_homepage()
+
+    log.info("Generating MP index...")
+    generate_mp_index()
+
+    log.info("Generating MP pages...")
+    generate_mp_pages()
+
+    log.info("Generating HTML session pages...")
+    generate_session_pages(fast_run)
+
     log.info("Generating session index...")
-    datedict = generate_datedict()
-    all_years = [y for y in datedict]
-    for year_number in datedict:
-        year = datedict[year_number]
-        context = {'year': year,
-                   'year_number': year_number,
-                   'all_years': all_years,
-                   'datedict': datedict,
-                   'months': MESES,
-                   'months_short': [m[:3] for m in MESES],
-                   'page_name': 'sessoes',
-                   }
-        target_dir = os.path.join(TRANSCRIPTS_PATH + "%s/" % year_number)
-        filename = target_dir + "index.html"
-        render_template_into_file(env, 'session_list.html', filename, context)
-    # Get most recent year and make the session index
-    y = all_years[-1]
-    year = datedict[y]
-    context = {'year': year,
-               'year_number': year_number,
-               'all_years': all_years,
-               'datedict': datedict,
-               'months': MESES,
-               'months_short': [m[:3] for m in MESES],
-               'page_name': 'sessoes',
-               }
-    render_template_into_file(env, 'session_list.html', TRANSCRIPTS_PATH + 'index.html', context)
+    generate_session_index()
 
 
 if __name__ == "__main__":
